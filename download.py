95352fdc1a8b2a6155b21fc22fa1a845c0cfe29b
Espen Sirnes <esi000@uit.no>
2024-04-23 10:42:29 +0200

    update

--- download.py
+++ download.py
@@ -10,17 +10,18 @@ import subprocess
 
 #Type the various names that the students can concivabley have called thir projects 
 #You should give them a specific name to use, but you can be certain that some students will mess it up
-REPO_NAMES=['COURSE-101', 'course-101']
+REPO_NAMES=['SOK-1005', 'sok-1005']
 
 
 #write the directory here. You can use os.getcwd() once to get the current directory, but since the 
 #working directory is changed in the script, DIRECTORY needs to be statically set:
-DIRECTORY='put working directory here!!' #os.getcwd()
+DIRECTORY = r"B:\Undervisning\SOK-1005\download_repo_assignments" #os.getcwd()
+START_ROW = 1
 
 #Go to https://github.com/settings/tokens/new and generate a token, and enter it here:
-TOKEN='Enter token here!!!'
+TOKEN=<your github token here>
 
-#Remember to fill usrnames.csv with two columns: the git hub user names of the students and a group identifier
+#Remember to fill usrnames.csv with two columns: the git hub user names of the students and a name identifier
 
 
 #        CODE:
@@ -34,12 +35,14 @@ revision_dates=[]
 
 def main():
     g=Github(TOKEN)
-    f = np.genfromtxt(DIRECTORY+'\\'+'usrnames.csv', delimiter=";",dtype=str,encoding='utf-8-sig')
-    for name,grp in f:
-        print(f'Fetching {name}, group {grp}')
+    f = np.genfromtxt(DIRECTORY+'\\'+r'usrnames.csv', delimiter=";",dtype=str,encoding='utf-8-sig')
+    for row in f[START_ROW:]:
+        un, name = [str(i) for i in row[:2]]
+        name = name.strip()
+        print(f'Fetching {un}, name {name}')
         try:
-            r=get_repo(g, name, REPO_NAMES)
-            path=DIRECTORY+'\\'+grp
+            r=get_repo(g, un, REPO_NAMES, row)
+            path=DIRECTORY+'\\'+name
             cloneurl=r.clone_url.replace('https://github',f'https://{TOKEN}@github')
             try:
                 os.chdir(path+'\\'+r.name)
@@ -50,21 +53,28 @@ def main():
                 os.chdir(path)
                 a=os.system(f"git clone {cloneurl}")
             rvdt=subprocess.getoutput("git log -1 --format=%cd")
-            revision_dates.append([grp,rvdt])
+            revision_dates.append([name,rvdt])
         except Exception as e:
             print(e)
-            print(f"Repository not found for {name}, group{grp}")
-            uncloned.append(name)
+            print(f"Repository not found for {un}, name{name}")
+            uncloned.append(un)
     os.chdir(DIRECTORY)
     np.savetxt('rev.csv',revision_dates,fmt='%s',delimiter=";")
     np.savetxt('uncloned.csv',uncloned,fmt='%s',delimiter=";")
     print(uncloned)
 
 
-def get_repo(g,name,reponames):
+def get_repo(g,un,reponames, row):
+    repo = None
+    if len(row)>2:
+        try:
+            r=g.get_user(un).get_repo(row[2])
+            return r
+        except:
+            pass
     for i in reponames:
         try:
-            r=g.get_user(name).get_repo(i)
+            r=g.get_user(un).get_repo(i)
             return r
         except:
             pass
--- fetch_links.py
+++ fetch_links.py
@@ -7,13 +7,13 @@ from bs4 import BeautifulSoup
 import csv
 import re
 
-PATH_TO_LINKS = r'<your link>'
+PATH_TO_LINKS = r'B:\Undervisning\SOK-1005\Innleveringer\Blotto V2024'
 
 def main():
 	# Prepare CSV file to write extracted data
 	f = open('usrnames2.csv', 'w', newline='', encoding='utf-8')
 	writer = csv.writer(f, delimiter= ';')
-	writer.writerow(['User', 'Group','Repository', 'Name', 'Title', 'URL'])  # Column headers
+	writer.writerow(['User', 'Name', 'Repository',  'Title', 'URL'])  # Column headers
 
 		# Iterate through all files in the directory
 	for filename in os.listdir(PATH_TO_LINKS):
@@ -31,19 +31,20 @@ def process(filename, writer):
 		soup = BeautifulSoup(f.read(), 'html.parser')
 
 		# Extract title and URL
-		title = soup.find('h1').text if soup.find('h1') else 'Title Not Found'
+		title = soup.find('h1').text if soup.find('h1') else 'Title_Not_Found'
 		link = soup.find('a')
-		url = link['href'] if link else 'URL Not Found'
+		url = link['href'] if link else 'URL_Not_Found'
 
 		# Extract user and repository from URL using regex
 		match = re.search(r'github\.com/([^/]+)/([^/?]+)', url)
 		if match:
 			user, repo = match.groups()
 		else:
-			user, repo = 'User Not Found', 'Repo Not Found'
-		title, name = title.split(':')
+			user, repo = 'User_Not_Found', 'Repo_Not_Found'
+		a = title.split(':')
+		title, name = ':'.join(a[:-1]), a[-1]
 		# Write the extracted data to the CSV file
-		writer.writerow([user, 0, repo, name, title, url])
+		writer.writerow([user, name, repo, title, url])
 
 
 
